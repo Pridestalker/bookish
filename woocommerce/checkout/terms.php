91d40ede890ba141d8c3f0f8d072f8b480b3dcc4
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-05-23 19:55:19 +0200

    Cleaned up terms.php

--- woocommerce/checkout/terms.php
+++ woocommerce/checkout/terms.php
@@ -1,40 +1,32 @@
 <?php
 /**
- * Checkout terms and conditions area.
- *
- * @package WooCommerce/Templates
  * @version 3.4.0
  */
 
-defined('ABSPATH') || exit;
+use Timber\Timber;
+use App\Helpers\Template;
 
-if (apply_filters('woocommerce_checkout_show_terms', true) && function_exists('wc_terms_and_conditions_checkbox_enabled')) {
-    do_action('woocommerce_checkout_before_terms_and_conditions');
+defined('ABSPATH') || exit(0);
 
-    ?>
-    <div class="woocommerce-terms-and-conditions-wrapper">
-        <?php
-        /**
-         * Terms and conditions hook used to inject content.
-         *
-         * @since 3.4.0.
-         * @hooked wc_checkout_privacy_policy_text() Shows custom privacy policy text. Priority 20.
-         * @hooked wc_terms_and_conditions_page_content() Shows t&c page content. Priority 30.
-         */
-        do_action('woocommerce_checkout_terms_and_conditions');
-        ?>
-
-        <?php if (wc_terms_and_conditions_checkbox_enabled()) : ?>
-            <p class="form-row validate-required">
-                <input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="terms" <?php checked(apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms'])), true); // WPCS: input var ok, csrf ok. ?> id="terms" />
-                <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox" for="terms">
-                    <span class="woocommerce-terms-and-conditions-checkbox-text"><?php wc_terms_and_conditions_checkbox_text(); ?></span>&nbsp;<span class="required">*</span>
-                </label>
-                <input type="hidden" name="terms-field" value="1" />
-            </p>
-        <?php endif; ?>
-    </div>
-    <?php
-
-    do_action('woocommerce_checkout_after_terms_and_conditions');
+if (!function_exists('wc_terms_and_conditions_checkbox_enabled') && apply_filters('woocommerce_checkout_show_terms', true)) {
+    return;
 }
+
+do_action('woocommerce_checkout_before_terms_and_conditions');
+
+$context = Timber::get_context();
+$context['enabled'] = wc_terms_and_conditions_checkbox_enabled();
+$context['checked'] = apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms']));
+$context['content_function'] = 'wc_terms_and_conditions_checkbox_text';
+
+$templates = [
+    Template::partialHtmlTwigFile('woocommerce/checkout/terms'),
+];
+
+Timber::render(
+    apply_filters('bookish/view-composer/front-page/context', $templates),
+    apply_filters('bookish/view-composer/front-page/context', $context),
+    apply_filters('bookish/view-composer/front-page/context', false)
+);
+
+do_action('woocommerce_checkout_after_terms_and_conditions');
