11b7d4f78c0630d974e9b68211803aea1b4d733d
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-05-23 18:25:48 +0200

    More cache busting

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,9 @@ const mix = require('laravel-mix');
 mix
   .js('assets/scripts/main.js', 'dist/scripts/main.js')
   .extract()
+	.webpackConfig({
+		output: {
+			chunkFilename: '[name].js?id=[chunkhash]',
+		}
+	})
   .disableNotifications();
