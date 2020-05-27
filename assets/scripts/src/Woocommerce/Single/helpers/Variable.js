51488fd166edad2b9e317806a3375d6d0edae151
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-05-27 19:17:06 +0200

    [wip] disable unavailable products

--- assets/scripts/src/Woocommerce/Single/helpers/Variable.js
+++ assets/scripts/src/Woocommerce/Single/helpers/Variable.js
@@ -30,6 +30,8 @@ export const isValidOption = (option, select, attributesMemo) => {
 		.filter(attribute => attribute.attributes.hasOwnProperty(attribute_name))
 		.filter(attribute => attribute.attributes[attribute_name] === option.value);
 
+	console.dir(attributes);
+
 	return attributes[0].is_in_stock;
 }
 
