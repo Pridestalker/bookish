04dd3942eb56ae814ac9d05bb223586e9a42d295
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-05 20:35:38 +0200

    working on micro frontends
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
+++ assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
@@ -6,7 +6,7 @@ export class SubmitButton extends Component {
 	render() {
 		return (
 			<button type='submit' name='add-to-cart' value={this.props.productID} className='product-add-to-cart' >
-				<FontAwesomeIcon icon={this.props.loading? faSpinner: faPlusHexagon} spin={this.propss.loading}/>
+				<FontAwesomeIcon icon={this.props.loading? faSpinner: faPlusHexagon} spin={this.props.loading}/>
 				<span>
 					Toevoegen aan winkelmand
 				</span>
