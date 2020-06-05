3f40ec32f31076b2a05b0f8453f792706dfeafc2
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-05 20:50:08 +0200

    working on micro frontends
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
+++ assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
@@ -5,10 +5,12 @@ import { faPlusHexagon, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
 export class SubmitButton extends Component {
 	render() {
 		return (
-			<button type='submit' name='add-to-cart' value={this.props.productID} className='product-add-to-cart' >
+			<button type='submit' name='add-to-cart' value={this.props.productID} className='product-add-to-cart' disabled={this.props.loading} >
 				<FontAwesomeIcon icon={this.props.loading? faSpinner: faPlusHexagon} spin={this.props.loading}/>
 				<span style={{ 'margin-left': '.5rem' }}>
-					Toevoegen aan winkelmand
+					{ this.props.loading?
+						'Aan het toevoegen':
+						'Toevoegen aan winkelmand'}
 				</span>
 			</button>
 		)
