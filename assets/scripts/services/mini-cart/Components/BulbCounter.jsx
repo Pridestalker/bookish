8f17296db578097ee8778a8bf1cda5b6beb669b1
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-05 17:05:42 +0200

    bugfixing
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/mini-cart/Components/BulbCounter.jsx
+++ assets/scripts/services/mini-cart/Components/BulbCounter.jsx
@@ -4,7 +4,9 @@ import { Component, render, h, Fragment } from 'preact'
 export class BulbCounter extends Component {
 	constructor(props) {
 		super(props);
+	}
 
+	componentDidMount() {
 		document.addEventListener('product-added-to-cart', (e) => {
 			console.dir(e);
 
