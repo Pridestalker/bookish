ad7db1b538a8f8b94d765ba0e3289b55b310b02f
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-05 20:16:49 +0200

    working on micro frontends
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
+++ assets/scripts/services/simple-add-to-cart/Components/SubmitButton.js
@@ -1,10 +1,12 @@
 import { Component, h } from 'preact';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusHexagon } from '@fortawesome/pro-duotone-svg-icons'
 
 export class SubmitButton extends Component {
 	render() {
 		return (
 			<button type='submit' name='add-to-cart' value={this.state.productID} className='product-add-to-cart' >
-				<i className='fad fa-plus-hexagon' />
+				<FontAwesomeIcon icon={faPlusHexagon} />
 				<span>
 					Toevoegen aan winkelmand
 				</span>
