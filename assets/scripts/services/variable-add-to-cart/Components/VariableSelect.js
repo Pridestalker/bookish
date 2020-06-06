ed114e1c4699e24187fe17cc5a5b0e3f539d3aa0
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-06 16:06:02 +0200

    adding variable add to cart app
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
+++ assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
@@ -3,7 +3,11 @@ import { Component, h } from 'preact'
 export class VariableSelect extends Component {
 	render() {
 		return (
-			<select onChange={this.props.changeHandler}>
+			<select onChange={this.props.changeHandler}
+					id={this.props.key}
+					name={this.props.key}
+					className={"block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"}
+			>
 				{ this.props.options.map((option, key) =>
 					<option value={option.toLowerCase()} key={key}>{option}</option>) }
 			</select>
