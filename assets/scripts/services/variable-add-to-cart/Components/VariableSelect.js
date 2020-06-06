7a9d2a7418c1221897114c4fd399bf143baf184f
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-06 16:41:04 +0200

    adding variable add to cart app
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
+++ assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
@@ -1,26 +1,30 @@
-import React, { Component, h } from 'preact';
+import React, { Component, h, Fragment } from 'preact';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/pro-solid-svg-icons';
 
 export class VariableSelect extends Component {
 	render() {
 		return (
-			<div className={'inline-block relative w-64'}>
+			<Fragment>
 				<label htmlFor={this.props.selectName}>
 					{this.props.selectName}
 				</label>
-				<select onChange={this.props.changeHandler}
-						id={this.props.selectName}
-						name={this.props.selectName}
-						className={"block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"}
-				>
-					{ this.props.options.map((option, key) =>
-						<option value={option.toLowerCase()} key={key}>{option}</option>) }
-				</select>
-				<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-					<FontAwesomeIcon icon={faChevronDown} />
+
+				<div className={'inline-block relative w-64'}>
+
+					<select onChange={this.props.changeHandler}
+							id={this.props.selectName}
+							name={this.props.selectName}
+							className={"block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"}
+					>
+						{ this.props.options.map((option, key) =>
+							<option value={option.toLowerCase()} key={key}>{option}</option>) }
+					</select>
+					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+						<FontAwesomeIcon icon={faChevronDown} />
+					</div>
 				</div>
-			</div>
+			</Fragment>
 		)
 	}
 }
