4d37b5f340a8bd74040b1a2b369ea238f6a7612a
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-06 17:20:07 +0200

    adding variable add to cart app
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
+++ assets/scripts/services/variable-add-to-cart/Components/VariableSelect.js
@@ -19,7 +19,7 @@ export class VariableSelect extends Component {
 							id={this.props.selectName}
 							name={this.props.selectName}
 							className={"block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"}
-							data-attribute_name={this.props.selectName}
+							data-attribute_name={`attribute_${this.props.selectName}`}
 					>
 						{ this.props.options.map((option, key) =>
 							<option value={option.toLowerCase()} key={key}>{option}</option>) }
