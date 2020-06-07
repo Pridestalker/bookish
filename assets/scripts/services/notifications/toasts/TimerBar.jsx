7299015fc42a9605b5559f29a39aa3b5c3668259
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-07 15:48:52 +0200

    Adding toast functionaility
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/services/notifications/toasts/TimerBar.jsx
+++ assets/scripts/services/notifications/toasts/TimerBar.jsx
@@ -7,6 +7,9 @@ bottom: 0;
 left: 0;
 width: ${props => (props.width || 0) + '%'};
 transition: width 225ms ease;
+height: 3px;
+background: rgba(255, 255, 255, 0.4);
+backdrop-filter: blur(2px);
 `
 
 export class TimerBar extends Component {
@@ -23,6 +26,8 @@ export class TimerBar extends Component {
 		if (!this.state.time) {
 			return <Fragment />
 		}
+
+		console.dir(this.props.time || 300)
 		return (
 			<ProgressBar width={this.state.width} />
 		);
