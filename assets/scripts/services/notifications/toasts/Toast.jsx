57e58479e1663bb18f5d010d7d6f2f3a7f37e61b
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-07 11:52:34 +0200

    Adding toast functionaility
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

---
+++ assets/scripts/services/notifications/toasts/Toast.jsx
@@ -0,0 +1,7 @@
+import React, { Component, h} from 'preact'
+
+export class Toast extends Component {
+	constructor(props) {
+		super(props);
+	}
+}
---
+++ assets/scripts/services/notifications/toasts/ToastWrapper.jsx
@@ -0,0 +1,55 @@
+import React, { Component, render, h } from 'preact';
+import { Toast } from './Toast'
+
+export class ToastWrapper extends Component {
+	constructor() {
+		super();
+
+		this.toastList = [];
+
+		this.findToasts = this.findToasts.bind(this);
+		this.createNewToast = this.createNewToast.bind(this);
+
+		this.findToasts();
+	}
+
+	findToasts() {
+		document.addEventListener('register-toast', evt => {
+			const {
+				title,
+				time,
+				content,
+				color,
+				actionText,
+				action
+			} = evt.details;
+
+			this.createNewToast(title, content, color, time, actionText, action);
+		})
+	}
+
+	createNewToast(title, content, color = 'primary', time = 5000, actionText = 'Close', action = function () {}) {
+		this.toastList.push(<Toast
+			title={title}
+			content={content}
+			color={color}
+			time={time}
+			actionText={actionText}
+			action={action}
+		/>)
+	}
+
+	render() {
+		return (
+			<aside>
+				{this.toastList}
+			</aside>
+		)
+	}
+}
+
+const toastContainer = document.querySelector('#toast-container');
+
+if (toastContainer) {
+	render(<ToastWrapper />, toastContainer);
+}
