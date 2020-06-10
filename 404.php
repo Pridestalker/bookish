20b0f1016db2e442d464983fa26f8817df3e90de
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-10 22:22:49 +0200

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+
+use Timber\Post;
+use Timber\Timber;
+use App\Helpers\Template;
+
+$context         = Timber::get_context();
+$post            = new Post();
+$context['post'] = $post;
+
+$templates = [
+	Template::viewHtmlTwigFile('404'),
+];
+
+return Timber::render(
+	apply_filters('bookish/view-composer/404-page/templates', $templates),
+	apply_filters('bookish/view-composer/404-page/context', $context),
+	apply_filters('bookish/view-composer/404-page/cache', false)
+);
