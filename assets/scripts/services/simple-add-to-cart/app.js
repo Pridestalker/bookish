1ece70abc68b2a5ffd3ab16437cd5e5f28c7a8ee
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-11 19:33:38 +0200

    Added quantity bumpers

--- assets/scripts/services/simple-add-to-cart/app.js
+++ assets/scripts/services/simple-add-to-cart/app.js
@@ -80,7 +80,7 @@ class AddToCart extends Component {
 		} else {
 			registerToast({
 				content: `Maar ${maxQuantity} beschikbaar`,
-				slug: 'max-beschikbaarheid',
+				slug: `max_availability-${new Date().getTime()}`,
 				time: 1500
 			})
 		}
