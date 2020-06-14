b778aa6b3203a9b055eb679e7ba9d3e5e32812d8
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-14 19:39:42 +0200

    fix for checkout?

--- src/Providers/AssetsServiceProvider.php
+++ src/Providers/AssetsServiceProvider.php
@@ -64,7 +64,7 @@ class AssetsServiceProvider extends ServiceProvider
     public function dequeueAssets(): void
     {
         if (!is_admin() || !is_cart() || !is_checkout()) {
-            WP::removeScript('jquery');
+//            WP::removeScript('jquery');
             WP::removeScript('wp-embed');
             WP::removeScript('hoverintent-js');
         }
