8107f0d429f3caecee19428ab70dcc2c0ee2c552
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-07-12 19:10:47 +0200

    removed debug point

--- assets/scripts/services/search-bar/app.js
+++ assets/scripts/services/search-bar/app.js
@@ -4,8 +4,6 @@ import { SearchBar } from './Components/Form';
 export function renderSearchBar() {
 	const searchBar = document.querySelectorAll('.pre-search-form');
 
-	window.alert('test');
-
 	for ( let i = 0; i < searchBar.length; i++ ) {
 		render(<SearchBar />, searchBar[i]);
 	}
