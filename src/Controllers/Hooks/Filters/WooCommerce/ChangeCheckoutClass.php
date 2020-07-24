f89c36b291e0210ce8300a10c7b83d7a48fc3e21
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-07-24 22:53:50 +0200

    removed extra billing field

--- src/Controllers/Hooks/Filters/WooCommerce/ChangeCheckoutClass.php
+++ src/Controllers/Hooks/Filters/WooCommerce/ChangeCheckoutClass.php
@@ -15,23 +15,23 @@ class ChangeCheckoutClass extends Filter
 
 	public function filter($fields = [])
     {
-        $fields['order']['billing_country']['input_class'][] = implode(',', [
-            'block',
-            'appearance-none',
-            'w-full',
-            'bg-white',
-            'border',
-            'border-gray-400',
-            'hover:border-gray-500',
-            'px-4',
-            'py-2',
-            'pr-8',
-            'rounded',
-            'shadow',
-            'leading-tight',
-            'focuse:outline-none',
-            'focus:shadow-outline',
-        ]);
+//        $fields['billing']['billing_country']['input_class'][] = implode(',', [
+//            'block',
+//            'appearance-none',
+//            'w-full',
+//            'bg-white',
+//            'border',
+//            'border-gray-400',
+//            'hover:border-gray-500',
+//            'px-4',
+//            'py-2',
+//            'pr-8',
+//            'rounded',
+//            'shadow',
+//            'leading-tight',
+//            'focuse:outline-none',
+//            'focus:shadow-outline',
+//        ]);
 
         return $fields;
     }
