8bdb5d6118ab1cc1f2f6bb50687b3969d005bd08
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-08-23 18:18:52 +0200

    [wip] migration to lit-elements

--- assets/scripts/src/Elements/Product/Gallery/gallery-group.js
+++ assets/scripts/src/Elements/Product/Gallery/gallery-group.js
@@ -2,6 +2,10 @@ import { LitElement, html, css, customElement, property} from 'lit-element'
 
 export class GalleryGroup extends LitElement {
 
+	createRenderRoot() {
+		return this;
+	}
+
 	static get properties() {
 		return {
 			selected: {
@@ -10,7 +14,16 @@ export class GalleryGroup extends LitElement {
 		}
 	}
 
+	get _slottedChildren() {
+		const slot = this.querySelectorAll('slot'),
+			childNodes = slot.assignedNodes({flatten: true});
+
+		return Array.prototype.filter.call(childNodes, (node) => node.nodeType == Node.ELEMENT_NODE);
+	}
+
 	render() {
+		console.dir(this._slottedChildren);
+
 		return html`<slot />`;
 	}
 }
